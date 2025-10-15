f8c7a1d5848f050bae4718f83726db0f49e9f1aa
fmlaugust <fmlaugust@gmail.com>
2025-10-14 20:22:33 -0500

    Upload files

---
+++ Class 1/script.js
@@ -0,0 +1,16 @@
+// Basic JavaScript for #100Devs Class 1 Homework
+// This is a simple placeholder since we haven't covered JS in class yet!
+
+// Console log to confirm the script is loaded
+console.log('🚀 #100Devs Class 1 Homework - Script Loaded!');
+
+// Log the current date
+console.log('Homework Due: October 17, 2025');
+
+// Simple welcome message when page loads
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('📚 Study Notes Page Loaded Successfully!');
+    console.log('💯 Keep grinding! You got this!');
+});
+
+// This is just a placeholder for now - we'll learn more JS in future classes!
